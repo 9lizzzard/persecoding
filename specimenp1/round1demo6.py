d6a683c2b3e6b08c4153ce160f17fc28448892a7
9lizzzard <125536362+9lizzzard@users.noreply.github.com>
2023-02-28 21:58:48 +0800

    Create round1demo6.py

---
+++ specimenp1/round1demo6.py
@@ -0,0 +1,11 @@
+runner1 = input()
+runner2 = input()
+runner3 = input()
+runner4 = input()
+runner5 = input()
+
+all_runners = [runner1, runner2, runner3, runner4, runner5]
+positions = ["1st", "2nd", "3rd", "4th", "5th"]
+
+position = all_runners.index("Ellie")
+print(positions[position])
