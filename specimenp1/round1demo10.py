87ca13fdaedd30aeaf4c1fcefb840c25bae3c195
9lizzzard <125536362+9lizzzard@users.noreply.github.com>
2023-03-07 16:46:04 +0800

    Update round1demo10.py

--- specimenp1/round1demo10.py
+++ specimenp1/round1demo10.py
@@ -1,30 +1,21 @@
-ingredients = {
-    "I": 2,
-    "M": 1,
-    "C": 3,
-    "W": 1,
-}
 ice_cream = 0
 milk = 0
 ice_cube = 0
-whipped_cream = 0
 
-input = input()
-for ingredient in input:
+ingredients = input()
+for ingredient in ingredients:
     if ingredient == "I":
         ice_cream += 1
     elif ingredient == "M":
         milk += 1
-    elif ingredient == "C":
-        ice_cube += 1
     else:
-        whipped_cream += 1
+        ice_cube += 1
 
-if ingredients["I"] > ice_cream:
+if 2 > ice_cream:
     print("I")
-elif ingredients["M"] > milk:
+elif 1 > milk:
     print("M")
-elif ingredients["C"] > ice_cube:
+elif 3 > ice_cube:
     print("C")
 else:
     print("W")
